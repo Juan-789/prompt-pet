39075583dcbcf12ff9ec794773e4cd02a0b55ac0
Juan-789 <juan.alfredoprincipal@gmail.com>
2026-01-06 23:57:45 -0500

    added the chomium ai npm package

--- pages/content-runtime/src/matches/callLLM/App.tsx
+++ pages/content-runtime/src/matches/callLLM/App.tsx
@@ -4,54 +4,8 @@ interface LLMResponse {
   "message": string;
 }
 
-async function betterPrompt(initPrompt: string): Promise<LLMResponse | undefined>{ //return string
-  const url: string = "http://127.0.0.1:5000/";
-  try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Backend is cooked ${response.status}`);
-    }
-    const result: LLMResponse = await response.json();
-    return result;
-  } catch (e) {
-    console.error(`shi got cooked twin in the callLLM betterPrompt func ${e}`);
-    return undefined;
-  }
-}
-
-async function searchAndScrapeGeminiOverview(query: string): Promise<string | undefined>{
-  //need to replace the whitespace with +
-  try {
-    let query_modified: string = query.replaceAll(" ","+");
-    const url: string = `https://www.google.com/search?q=${query_modified}+gemini+overview`
-
-    chrome.tabs.create({
-      url: url
-    })
-    return "great success";
-  } catch (e) {
-    console.log(`error in searchAndScrape service worker ${e}`);
-    return undefined;
-  }
-}
 
 export default function App() {
-  // const messageHandler = useCallback((msg, sender, sendResponse) => {
-  //   if (msg.action === 'USER_PROMPT') {
-  //     console.log("Content Script received message with new prompt:", msg.dataToSend);        
-  //     sendResponse({ status: "success", received: true }); 
-  //     return true; 
-  //   }
-  // }, []); 
-  // chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  //   if (message.action === 'SCRAPE_OVERVIEW') {
-  //     console.log("received new tab request");
-  //     searchAndScrapeGeminiOverview(message.dataToSend);
-  //     sendResponse({ status: "success", received: true});
-  //     return true;
-  //   }
-  // }
-  // );
   const [data, setData] = useState<LLMResponse | null>(null);
   const [loading, setLoading] = useState(true);
 
